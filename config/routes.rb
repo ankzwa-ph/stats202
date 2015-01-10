87c53e81ddf31bab200a2f3c928905753ee74a77
Akira Nakazawa <akira.nakazawa@bibo.com.ph>
2015-01-11 02:05:24 +0800

    tiny changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :task_stats
+  root 'task_stats#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
