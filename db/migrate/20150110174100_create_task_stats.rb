ea66b591e1cd9bdd54cfb7e67600d1eb014e4a23
Akira Nakazawa <akira.nakazawa@bibo.com.ph>
2015-01-11 01:44:41 +0800

    Add null: false

--- db/migrate/20150110174100_create_task_stats.rb
+++ db/migrate/20150110174100_create_task_stats.rb
@@ -1,12 +1,12 @@
 class CreateTaskStats < ActiveRecord::Migration
   def change
     create_table :task_stats do |t|
-      t.integer :open
-      t.integer :in_progress
-      t.integer :resolved
-      t.integer :closed
-      t.integer :total
-      t.date :processing_date
+      t.integer :open, null: false
+      t.integer :in_progress, null: false
+      t.integer :resolved, null: false
+      t.integer :closed, null: false
+      t.integer :total, null: false
+      t.date :processing_date, null: false
 
       t.timestamps null: false
     end
