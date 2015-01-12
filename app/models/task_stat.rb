a9534dd24ed15240574a0c981d82829793b140f5
Akira Nakazawa <akira.nakazawa@bibo.com.ph>
2015-01-13 00:31:20 +0800

    Remove redundant code and use Array#transpose

--- app/models/task_stat.rb
+++ app/models/task_stat.rb
@@ -1,20 +1,9 @@
 class TaskStat < ActiveRecord::Base
 
-  def self.transpose_table(task_stats)
-    table = []
-    row_items = [:processing_date] + TaskStatus.all.map(&:name)
-    row_items.each do |status|
-      list = []
-      task_stats.each do |item|
-        if status == :processing_date
-          list << item[status].strftime("%d")
-        else
-          list << item[status]
-        end
-      end
-      table << list
-    end
-    row_items[0] = ''
-    table.map { |x| [row_items.shift] + x }
+  def self.transpose_table(table)
+    matrix = table.map { |e| [e.open, e.in_progress, e.resolved, e.closed, e.total] }
+    matrix = matrix.transpose
+    statuses = [''] + TaskStatus.all.map(&:name)
+    matrix.map { |m| [statuses.shift] + m }
   end
 end
