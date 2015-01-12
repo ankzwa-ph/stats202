25716addfa4a94afc501ce1dcf1bbf3afb4068bb
Akira Nakazawa <akira.nakazawa@bibo.com.ph>
2015-01-13 02:04:22 +0800

    Add a row to spreadsheet

--- app/models/task_stat.rb
+++ app/models/task_stat.rb
@@ -1,7 +1,11 @@
 class TaskStat < ActiveRecord::Base
 
   def self.transpose_table(table)
-    matrix = table.map { |e| [e.open, e.in_progress, e.resolved, e.closed, e.total] }
+    matrix = table.map do |e|
+      date = e.processing_date
+      str = ([1,15].include? date.day) ? "%m-%d" : "%d"
+      [date.strftime(str), e.open, e.in_progress, e.resolved, e.closed, e.total]
+    end
     matrix = matrix.transpose
     statuses = [''] + TaskStatus.all.map(&:name)
     matrix.map { |m| [statuses.shift] + m }
--- app/models/task_status.rb
+++ app/models/task_status.rb
@@ -4,6 +4,7 @@ class TaskStatus < ActiveHash::Base
       { id: 1, name: :open },
       { id: 2, name: :in_progress },
       { id: 3, name: :resolved },
-      { id: 4, name: :closed }
+      { id: 4, name: :closed },
+      { id: 5, name: :total }
     ]
 end
