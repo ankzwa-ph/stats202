86770d4821fb4d0c85626850172ef7579333dcf0
Akira Nakazawa <akira.nakazawa@bibo.com.ph>
2015-01-19 01:37:27 +0800

    Rename method

--- app/helpers/task_lists_helper.rb
+++ app/helpers/task_lists_helper.rb
@@ -1,6 +1,6 @@
 module TaskListsHelper
-  def get_parent_issue(task)
+  def get_parent(id)
     url = Settings.backlog.base_url + Settings.backlog.api.issue + '?apiKey=%s'
-    HTTParty.get(url % [task['parentIssueId'], ENV['BACKLOG_API_KEY']])
+    HTTParty.get(url % [id, ENV['BACKLOG_API_KEY']])
   end
 end
