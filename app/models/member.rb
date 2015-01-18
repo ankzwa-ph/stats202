56cd627d056118b847bf5484e323498a4736b225
Akira Nakazawa <akira.nakazawa@bibo.com.ph>
2015-01-18 14:02:53 +0800

    Add member class

---
+++ app/models/member.rb
@@ -0,0 +1,18 @@
+class Member < ActiveHash::Base
+  self.data =
+    [
+      { id: 0, backlog_id: 1073913157, name: 'Clet' },
+      { id: 1, backlog_id: 1073913168, name: 'Melvin' },
+      { id: 2, backlog_id: 1073913158, name: 'Angelo' },
+      { id: 3, backlog_id: 1073913165, name: 'Paw' },
+      { id: 4, backlog_id: 1073913164, name: 'Lymuel' },
+      { id: 5, backlog_id: 1073935499, name: 'Ralph' },
+      { id: 6, backlog_id: 1073940965, name: 'Naoya' },
+      { id: 7, backlog_id: 1073946119, name: 'Gretchen' },
+      { id: 8, backlog_id: 1073952144, name: 'Jeff' },
+      { id: 9, backlog_id: 1073955528, name: 'Andy' },
+      { id: 10, backlog_id: 1073913161, name: 'Cel' },
+      { id: 11, backlog_id: 1073941726, name: 'Akira' }
+    ]
+
+end
