29487cca3b4dc8419bd05af866d529ff7e490039
Akira Nakazawa <akira.nakazawa@bibo.com.ph>
2015-01-19 00:04:30 +0800

    Add module BacklogApiAccessible

---
+++ app/controllers/concerns/backlog_api_accessible.rb
@@ -0,0 +1,19 @@
+module BacklogApiAccessible
+  extend ActiveSupport::Concern
+
+  included do
+    def fetch(path, params = {})
+      url = Settings.backlog.base_url + path + url_generate(params)
+      HTTParty.get(url)
+    end
+  end
+
+  private
+    def url_generate(params)
+      res = '?apiKey=%s&projectId[]=1073827573&' % ENV['BACKLOG_API_KEY']
+      res += params.map { |x| x.join('=') }.join('&')
+    end
+
+  module ClassMethod
+  end
+end
--- app/controllers/task_lists_controller.rb
+++ app/controllers/task_lists_controller.rb
@@ -1,6 +1,10 @@
 class TaskListsController < ApplicationController
+  include BacklogApiAccessible
+
   def index
-    uri = Settings.backlog.base_url + Settings.backlog.api.issues + '?apiKey=%s&projectId[]=1073827573&assigneeId[]=%s&statusId[]=1&statusId[]=2'
-    @tasks = Member.all.map(&:backlog_id).map { |id| HTTParty.get(uri % [ENV['BACKLOG_API_KEY'], id]) }
+    params = { 'statusId[]' => 1, 'statusId[]' => 2 }
+    @tasks = Member.all.map(&:backlog_id).map do |id|
+      fetch(Settings.backlog.api.issues, params.merge({'assigneeId[]' => id}))
+    end
   end
 end
