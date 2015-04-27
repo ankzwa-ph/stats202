7a8fca2119dba7908d3beaea33fcee6ba8aef89e
Akira Nakazawa <akira.nakazawa@bibo.com.ph>
2015-04-27 20:18:53 +0800

    update member

--- app/models/member.rb
+++ app/models/member.rb
@@ -8,12 +8,10 @@ class Member < ActiveHash::Base
       { id: 4, backlog_id: 1073913164, name: 'Lymuel' },
       { id: 5, backlog_id: 1073935499, name: 'Ralph' },
       { id: 6, backlog_id: 1073940965, name: 'Naoya' },
-      { id: 7, backlog_id: 1073946119, name: 'Gretchen' },
-      { id: 8, backlog_id: 1073952144, name: 'Jeff' },
-      { id: 9, backlog_id: 1073955528, name: 'Andy' },
       { id: 10, backlog_id: 1073913161, name: 'Cel' },
       { id: 11, backlog_id: 1073941726, name: 'Akira' },
       { id: 12, backlog_id: 1073913163, name: 'John' },
-      { id: 13, backlog_id: 1073913218, name: 'Jarl' }
+      { id: 13, backlog_id: 1073913218, name: 'Jarl' },
+      { id: 14, backlog_id: 1073969958, name: 'kevin' }
     ]
 end
