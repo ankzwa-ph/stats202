b320992dc717f967ff588f53de3fef67eddc9fc5
Akira Nakazawa <akira.nakazawa@bibo.com.ph>
2015-04-28 21:46:29 +0800

    set default date range

--- app/models/search/task_stat.rb
+++ app/models/search/task_stat.rb
@@ -8,6 +8,7 @@ class Search::TaskStat < Search::Base
   def matches
     t = ::TaskStat.arel_table
     results = TaskStat.all
+    results = results.where(t[:processing_date].gteq(Date.today.prev_month)) if date_from.nil? && date_to.nil?
     results = results.where(t[:processing_date].gteq("#{date_from}")) if date_from.present?
     results = results.where(t[:processing_date].lteq("#{date_to}")) if date_to.present?
     results
